8addadb2357a129130c2f3132a83e0d4de9f46f5
TricioTM <ptanusmo@itam.mx>
2025-02-27 12:34:11 -0600

    Update and rename page.tsx.txt to page.tsx

---
+++ todo/app/protected/page.tsx
@@ -0,0 +1,13 @@
+import AddToDo from "@/components/AddToDo"; 
+import ToDoList from "@/components/ToDoList"; 
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <AddtoDo />
+      <ToDoList />
+    </div>
+  );
+}
